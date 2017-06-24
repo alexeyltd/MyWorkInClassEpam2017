b1229bdf6b66b86850df960797b961a474dd1c12
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-06-24 14:04:41 +0300

    included critical values

--- src/lesson170624/mywork/Max.java
+++ src/lesson170624/mywork/Max.java
@@ -1,5 +1,6 @@
 package lesson170624.mywork;
 
+// Class for test maximus values
 public class Max {
 
 	public static void main(String[] args) {
@@ -7,22 +8,12 @@ public class Max {
 //		Equals max
 		System.out.println(SimpleUnit.assertEquals(MathsUtils.max(10, 30), 30));
 		System.out.println(SimpleUnit.assertEquals(MathsUtils.max(2000, 3000), 3000));
-		System.out.println(SimpleUnit.assertEquals(MathsUtils.max(5000, 300), 5000));
+		System.out.println(SimpleUnit.assertEquals(MathsUtils.max(Integer.MAX_VALUE, Integer.MIN_VALUE), Integer.MAX_VALUE));
 
 //		Not equals max
 		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.max(30, 10), 10));
 		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.max(100, 600), 100));
-		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.max(5000, 10000), 5000));
-
-//		Equals min
-		System.out.println(SimpleUnit.assertEquals(MathsUtils.min(5, 7), 5));
-		System.out.println(SimpleUnit.assertEquals(MathsUtils.min(300, 1), 1));
-		System.out.println(SimpleUnit.assertEquals(MathsUtils.min(3000, 5000), 3000));
-
-//		Not equals min
-		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.min(40, 100), 100));
-		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.min(550, 560), 560));
-		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.min(3450, 5450), 5450));
+		System.out.println(SimpleUnit.assertNotEquals(MathsUtils.max(Integer.MAX_VALUE, Integer.MIN_VALUE), Integer.MIN_VALUE));
 
 	}
 
