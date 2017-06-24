c89de17502d776d303fe227a5258c3225b63d866
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-06-24 11:51:46 +0300

    added function

--- src/lesson170624/FizzBuzz.java
+++ src/lesson170624/FizzBuzz.java
@@ -5,9 +5,21 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		System.out.println("start");
+		
 		System.out.println(assertEquals("Hello", "Hello"));
 		System.out.println(assertEquals("Hello", "Hello "));
+
+		String result = fizzBuzz(3);
+
+		System.out.println(assertEquals(result, "Fizz"));
+
+
 	}
+
+	private static String fizzBuzz(int i) {
+		return null;
+	}
+
 
 	public static boolean assertEquals(String actual, String expected) {
 		return actual.equals(expected);
