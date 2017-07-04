114617d56a782be5876892718eae7a6843632deb
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-07-04 10:45:06 +0300

    added tests for homework

--- src/lesson170701/myworkathome/SumInMatrix.java
+++ src/lesson170701/myworkathome/SumInMatrix.java
@@ -4,15 +4,7 @@ public class SumInMatrix {
 
 	private static final int MATRIX_SIZE = 10;
 
-	public static void main(String[] args) {
-
-		int[][] matrix = MatrixGenerator.generate(MATRIX_SIZE);
-
-		System.out.println(countPositive(matrix));
-
-	}
-
-	private static int countPositive(int[][] matrix) {
+	public static int countPositive(int[][] matrix) {
 		int sum = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			int[] rows = matrix[i];
---
+++ src/lesson170701/myworkathome/SumInMatrixUtils.java
@@ -0,0 +1,13 @@
+package lesson170701.myworkathome;
+
+public class SumInMatrixUtils {
+
+	public static boolean assertEquals(int i, int j) {
+		return i == j;
+	}
+
+	public static boolean assertNotEquals(int i, int j) {
+		return i != j;
+	}
+
+}
