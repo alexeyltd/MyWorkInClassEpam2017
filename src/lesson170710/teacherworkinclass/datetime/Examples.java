fc71cdee926034ba3bac09644eb3d616154e35f8
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-07-10 14:51:50 +0300

    classwork

---
+++ src/lesson170710/teacherworkinclass/datetime/Examples.java
@@ -0,0 +1,28 @@
+package lesson170710.teacherworkinclass.datetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Examples {
+
+	public static void main(String[] args) {
+
+		Date date = new Date();
+
+		System.out.println(date);
+
+		System.out.println(Calendar.getInstance().getTime());
+
+		System.out.println(LocalTime.now());
+		System.out.println(LocalDate.now());
+		System.out.println(LocalDateTime.now());
+
+
+
+
+	}
+
+}
