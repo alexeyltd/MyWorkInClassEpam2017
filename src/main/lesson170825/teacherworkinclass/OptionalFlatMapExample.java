c639a0ae0d86c50b9a38f1a50e82b01f78e63ff6
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-08-31 18:12:55 +0300

    new class work

--- src/main/lesson170825/teacherworkinclass/OptionalFlatMapExample.java
+++ src/main/lesson170825/teacherworkinclass/OptionalFlatMapExample.java
@@ -6,12 +6,51 @@ public class OptionalFlatMapExample {
 
 	public static void main(String[] args) {
 
-		String s = null;
+		Person p = new Person();
+		Car car = new Car();
+		Insurance insurance = new Insurance("Renessance");
 
-		Optional<String> stringOptional = Optional.ofNullable(s);
+		car.insurance = Optional.of(insurance);
+		p.car = Optional.of(car);
 
-		Optional<Integer> integerOptional = stringOptional.map(String::length);
+		System.out.println(getCarInsuranceName(null));
+		System.out.println(getCarInsuranceName(new Person()));
+		System.out.println(getCarInsuranceName(p));
+
 
 	}
 
+	public static String getCarInsuranceName(Person person) {
+		return Optional.ofNullable(person).flatMap(Person::getCar).flatMap(Car::getInsurance)
+				.map(Insurance::getName).orElse("Unknown");
+	}
+
+}
+
+class Person {
+	Optional<Car> car = Optional.empty();
+
+	public Optional<Car> getCar() {
+		return car;
+	}
+}
+
+class Car {
+	Optional<Insurance> insurance = Optional.empty();
+
+	public Optional<Insurance> getInsurance() {
+		return insurance;
+	}
+}
+
+class Insurance {
+	String name;
+
+	public Insurance(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
